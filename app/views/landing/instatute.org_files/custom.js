ac1f5caa815c9e98335527a9b5b9f2e58091e7cd
achafamo <achafamo@colgate.edu>
2017-07-06 12:39:50 -0400

    pray

---
+++ app/views/landing/instatute.org_files/custom.js
@@ -0,0 +1,56 @@
+window.sr = ScrollReveal();
+sr.reveal('.foo'), {
+    duration: 200
+    , scale: 0.9
+};
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggler").click();
+        }
+    });
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+    $('#apply').on('click', function () {
+        window.location = "http://instatute.org/security_check/";
+        /*$.amaran({
+            content: {
+                bgcolor: '#06222E'
+                , color: '#fff'
+                , message: 'Coming Soon!'
+            }
+            , theme: 'colorful'
+        });*/
+    });
+    $('#apply2').on('click', function () {
+        window.location = "http://instatute.org/security_check/";
+        /*$.amaran({
+            content: {
+                bgcolor: '#06222E'
+                , color: '#fff'
+                , message: 'Coming Soon!'
+            }
+            , theme: 'colorful'
+        });*/
+    });
+    $('#apply3').on('click', function () {
+        window.location = "http://instatute.org/security_check/";
+        /*$.amaran({
+            content: {
+                bgcolor: '#06222E'
+                , color: '#fff'
+                , message: 'Coming Soon!'
+            }
+            , theme: 'colorful'
+        });*/
+    });
+});
