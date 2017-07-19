4544bebfea645dad3c35e69d2cfafb6a532caa42
achafamo <achafamo@colgate.edu>
2017-07-19 18:39:43 -0400

    added google calendar

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController   
-	def google_oauth2
-	     
+	def google_oauth2	     
 	  @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-
 	  if @user.persisted?
 	    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
 	    sign_in_and_redirect @user, :event => :authentication
@@ -11,4 +9,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	    redirect_to new_user_registration_url
 	  end
 	end
+
 end
